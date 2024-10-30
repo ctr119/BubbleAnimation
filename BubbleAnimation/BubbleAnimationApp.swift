fcb1ea4b49bd93659e910613cacc544859db5a6d
Victor Barrios <victor@beiningbogen.no>
2024-10-30 13:23:58 +0100

    Initial Commit

---
+++ BubbleAnimation/BubbleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleAnimationApp.swift
+//  BubbleAnimation
+//
+//  Created by Victor Barrios Sanchez on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubbleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
